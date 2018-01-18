c1c51b8f8ae6a1d5e7d79574e556d337e7f2c185
zhongguoxin <zhongguoxin@meituan.com>
2018-01-18 16:07:37 +0800

    1.2.5,调整export

--- index.d.ts
+++ index.d.ts
@@ -5,10 +5,9 @@ export interface IPostRoute {}
 
 import axios, {AxiosRequestConfig} from 'axios';
 
-export default class Request {
-  get<URL extends keyof IGetRoute>(url: URL, config?: AxiosRequestConfig): IGetRoute[URL];
-  post<URL extends keyof IPostRoute>(url: URL, data?: any, config?: AxiosRequestConfig): IPostRoute[URL];
-}
+export function get<URL extends keyof IGetRoute>(url: URL, config?: AxiosRequestConfig): IGetRoute[URL];
+
+export function post<URL extends keyof IPostRoute>(url: URL, data?: any, config?: AxiosRequestConfig): IPostRoute[URL];
 
 export interface IHttpError extends Error {
   code: number;
--- index.ts
+++ index.ts
@@ -8,30 +8,29 @@ import axios, {AxiosRequestConfig} from 'axios';
 /**
  * 目前提供 get 和 post 两种方法
  */
-export default class Request {
-  async get<URL extends keyof IGetRoute>(url: URL, config?: AxiosRequestConfig): Promise<any> {
-    const res =  await axios.get(url, config);
-    if (!res) {
-      throw new Error('请求失败，没有数据返回');
-    }
-    const {code, data} = res.data;
-    if (code) {
-      throw data;
-    } else {
-      return data;
-    }
-  }
 
-  async post<URL extends keyof IPostRoute>(url: URL, postdata?: any, config?: AxiosRequestConfig): Promise<any> {
-    const res = await axios.post(url, postdata, config);
-    if (!res) {
-      throw new Error('请求失败，没有数据返回');
-    }
-    const {code, data} = res.data;
-    if (code) {
-      throw data;
-    } else {
-      return data;
-    }
+ export async function get<URL extends keyof IGetRoute>(url: URL, config?: AxiosRequestConfig): Promise<any> {
+  const res =  await axios.get(url, config);
+  if (!res) {
+    throw new Error('请求失败，没有数据返回');
+  }
+  const {code, data} = res.data;
+  if (code) {
+    throw data;
+  } else {
+    return data;
+  }
+}
+
+export async function post<URL extends keyof IPostRoute>(url: URL, postdata?: any, config?: AxiosRequestConfig): Promise<any> {
+  const res = await axios.post(url, postdata, config);
+  if (!res) {
+    throw new Error('请求失败，没有数据返回');
+  }
+  const {code, data} = res.data;
+  if (code) {
+    throw data;
+  } else {
+    return data;
   }
 }
