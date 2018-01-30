331946ee960ff59a9a3784c4544f9d69090432ec
zhongguoxin <zhongguoxin@meituan.com>
2018-01-30 17:07:18 +0800

    feat(interceptors): 统一 loading，以及登录态失效

--- index.d.ts
+++ index.d.ts
@@ -3,14 +3,20 @@ export interface IGetRoute {}
 
 export interface IPostRoute {}
 
-import axios, {AxiosRequestConfig} from 'axios';
+interface IConfig extends AxiosRequestConfig {
+  loadingText?: string;
+}
 
-export function get<URL extends keyof IGetRoute>(url: URL, config?: AxiosRequestConfig): IGetRoute[URL];
+import axios, {AxiosRequestConfig, AxiosStatic} from 'axios';
 
-export function post<URL extends keyof IPostRoute>(url: URL, data?: any, config?: AxiosRequestConfig): IPostRoute[URL];
+export function get<URL extends keyof IGetRoute>(url: URL, config?: IConfig): IGetRoute[URL];
+
+export function post<URL extends keyof IPostRoute>(url: URL, data?: any, config?: IConfig): IPostRoute[URL];
 
 export interface IHttpError extends Error {
   code: number;
   data: any;
   msg: string;
 }
+
+export const instance: AxiosStatic;
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { Loading } from '@mtfe/mcashier-components';
+import * as KNB from '@dp/knb/titans';
 
 export interface IGetRoute {}
 
@@ -5,14 +7,20 @@ export interface IPostRoute {}
 
 import axios, {AxiosRequestConfig} from 'axios';
 
+export const instance = axios;
+
+interface IConfig extends AxiosRequestConfig {
+  loadingText: string;
+}
+
 /**
  * 目前提供 get 和 post 两种方法
  */
 
- export async function get<URL extends keyof IGetRoute>(url: URL, config?: AxiosRequestConfig): Promise<any> {
+ export async function get<URL extends keyof IGetRoute>(url: URL, config?: IConfig): Promise<any> {
   const res =  await axios.get(url, config);
   if (!res) {
-    throw { msg: '请求失败，没有数据返回' };
+    throw { msg: '获取数据失败，请重试' };
   }
   const {code, data} = res.data;
   if (code) {
@@ -22,10 +30,10 @@ import axios, {AxiosRequestConfig} from 'axios';
   }
 }
 
-export async function post<URL extends keyof IPostRoute>(url: URL, postdata?: any, config?: AxiosRequestConfig): Promise<any> {
+export async function post<URL extends keyof IPostRoute>(url: URL, postdata?: any, config?: IConfig): Promise<any> {
   const res = await axios.post(url, postdata, config);
   if (!res) {
-    throw { msg: '请求失败，没有数据返回' };
+    throw { msg: '获取数据失败，请重试' };
   }
   const {code, data} = res.data;
   if (code) {
@@ -34,3 +42,59 @@ export async function post<URL extends keyof IPostRoute>(url: URL, postdata?: an
     return data;
   }
 }
+
+// export async function put<URL extends keyof IPostRoute>(url: URL, postdata?: any, config?: IConfig): Promise<any> {
+//   const res = await axios.put(url, postdata, config);
+//   if (!res) {
+//     throw { msg: '获取数据失败，请重试' };
+//   }
+//   const {code, data} = res.data;
+//   if (code) {
+//     throw res;
+//   } else {
+//     return data;
+//   }
+// }
+
+// export async function dlt<URL extends keyof IPostRoute>(url: URL, config?: IConfig): Promise<any> {
+//   const res = await axios.delete(url, config);
+//   if (!res) {
+//     throw { msg: '获取数据失败，请重试' };
+//   }
+//   const {code, data} = res.data;
+//   if (code) {
+//     throw res;
+//   } else {
+//     return data;
+//   }
+// }
+
+
+
+let close;
+
+axios.interceptors.request.use((config: IConfig) => {
+  console.log('[http-rpc-client]call request interceptors');
+  if (config.loadingText) {
+    close = Loading.loading(config.loadingText);
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use((response) => {
+  console.log('[http-rpc-client]call response interceptors');
+  if (close) {
+    close();
+  }
+  if (response && response.data && String(response.data.code) === '401') {
+    return KNB.login();
+  }
+  return response;
+}, (error) => {
+  if (close) {
+    close();
+  }
+  return Promise.reject(error);
+});
